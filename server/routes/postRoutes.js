7eae233b470070fd10853013e1fc97c3701fd6e1
Namit-me <namitme2002@gmail.com>
2023-07-22 17:26:20 +0530

    namit

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from 'express';
 import * as dotenv from 'dotenv';
 import { v2 as cloudinary } from 'cloudinary';
 
@@ -9,38 +9,35 @@ dotenv.config();
 const router = express.Router();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-
-//GET ALL POSTS
-router.route('/').get(async(req,res)=>{
-    try {
-        const posts = await Post.find({});
-
-        res.status(200).json({success: true, data:posts})
-    } catch (error) {
-        res.status(500).json({success: false, message:error})
-        
-    }
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-// CREATE A POST
-router.route('/').post(async(req,res)=>{
-try {
+
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  try {
     const { name, prompt, photo } = req.body;
-    const photoUrl = await cloudinary.uploader(photo);
+    const photoUrl = await cloudinary.uploader.upload(photo);
 
     const newPost = await Post.create({
-        name,
-        prompt,
-        photo: photoUrl.url,
-    })
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
 
-    res.status(201).json({success: true, data:newPost });
-} catch (error) {
-    res.status(500).json({success: false, message: error})
-}
+    res.status(200).json({ success: true, data: newPost });
+  } catch (err) {
+    res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+  }
 });
 
 export default router;
